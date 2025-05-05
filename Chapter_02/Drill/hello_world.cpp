26076f7f4d0259081295f9f1a96209118c428220
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-05-05 20:36:52 +0100

    Add project setup and main source file

---
+++ Chapter_02/Drill/hello_world.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Stefan Diaconu on 05/05/2025.
+//
+
+#include "../../std_lib_facilities.h"
+
+int main()  // C++ programs start by executing the function main
+{
+    cout << "Hello, World!\n";  // output "Hello, World!"
+    return 0;
+}
