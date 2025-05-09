1ba2b5f0fb12c251503b925718b60526a0d9d04b
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-05-09 20:57:09 +0100

    Exercise 2 solution

---
+++ Chapter_02/Exercise_02/exercise_02.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Stefan Diaconu on 09/05/2025.
+//
+
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, programming!\n";
+    cout << "Here we go!\n";
+    return 0;
+}
