d87b2ef5dd6bf01c78d9c5344805618e88f2e7e1
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-05-21 20:29:13 +0100

    Complete exercise 2

---
+++ Chapter_03/Exercise_02/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Stefan Diaconu on 21/05/2025.
+//
+
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+    cout << "Please enter the number of miles: ";
+    double miles;
+    cin >> miles;
+    cout << miles << " in kilometers is " << miles * 1.609;
+
+    return 0;
+}
