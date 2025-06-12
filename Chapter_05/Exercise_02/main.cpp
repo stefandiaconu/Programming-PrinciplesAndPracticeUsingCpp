818fc175a24a2d7ab43e1d4275f0eb86621e986e
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-06-12 21:56:39 +0100

    Complete exercise 4

--- Chapter_05/Exercise_02/main.cpp
+++ Chapter_05/Exercise_02/main.cpp
@@ -7,6 +7,12 @@
 double ctok(const double c)
 {
     const double k = c + 273.15;
+
+    if (c < -273.15) {
+        cerr << "Input below absolute temperature that can be reached!\n";
+        throw exception();
+    }
+
     return k;
 }
 
@@ -14,12 +20,13 @@ int main ()
 {
     double c = 0;
     cin >> c;
-    if (c < -273.15) {
-        cerr << "Input below absolute temperature that can be reached!\n";
+    try {
+        const double k = ctok(c);
+        cout << k << '\n';
+    }
+    catch (...) {
         return 1;
     }
-    const double k = ctok(c);
-    cout << k << '\n';
 
     return 0;
 }
