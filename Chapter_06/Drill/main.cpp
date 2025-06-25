4838cc87cb5f3ecaadc882662763b17225730dc4
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-06-25 20:21:17 +0100

    Complete Drill exercise

--- Chapter_06/Drill/main.cpp
+++ Chapter_06/Drill/main.cpp
@@ -78,8 +78,8 @@ Token Token_stream::get()
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
 
     switch (ch) {
-    case ';':    // for "print"
-    case 'q':    // for "quit"
+    case '=':    // for "print"
+    case 'x':    // for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/':
         return Token(ch);        // let each character represent itself
     case '.':
@@ -184,12 +184,13 @@ double expression()
 int main()
 try
 {
+    cout << "Welcome to our simple calculator.\nPlease enter expressions using floating-point numbers." << endl;
     double val = 0;
     while (cin) {
         Token t = ts.get();
 
-        if (t.kind == 'q') break; // 'q' for quit
-        if (t.kind == ';')        // ';' for "print now"
+        if (t.kind == 'x') break; // 'q' for quit
+        if (t.kind == '=')        // ';' for "print now"
             cout << "=" << val << '\n';
         else
             ts.putback(t);
