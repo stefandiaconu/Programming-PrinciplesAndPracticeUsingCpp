4dd654282021d05374844c81ed6c7ddd49889153
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-07-26 21:58:27 +0100

    Complete exercise 2 of the Drill

--- Chapter_08/Drill/main.cpp
+++ Chapter_08/Drill/main.cpp
@@ -2,14 +2,45 @@
 // Created by Stefan Diaconu on 25/07/2025.
 //
 
+#include <iostream>
+using namespace std;
+
 #include "my.h"
 
 int main()
 {
     // Exercise 1
-    foo = 7;
-    print_foo();
-    print(99);
+    // foo = 7;
+    // print_foo();
+    // print(99);
+
+    // Exercise 2
+    int x = 7;
+    int y = 9;
+    swap_v(x,y);
+    cout << "x = " << x << " y = " << y << endl;
+    swap_r(x,y);
+    cout << "x = " << x << " y = " << y << endl;
+    swap_cr(x,y);
+    cout << "x = " << x << " y = " << y << endl;
+    swap_v(7,9);
+    // swap_r(7,9);
+    swap_cr(7,9);
+    const int cx = 7;
+    const int cy = 9;
+    swap_v(cx,cy);
+    cout << "cx = " << cx << " cy = " << cy << endl;
+    // swap_r(cx,cy);
+    swap_cr(cx,cy);
+    cout << "cx = " << cx << " cy = " << cy << endl;
+    swap_v(7.7,9.9);
+    double dx = 7.7;
+    double dy = 9.9;
+    swap_v(dx,dy);
+    cout << "dx = " << dx << " dy = " << dy << endl;
+    // swap_r(dx,dy);
+    swap_cr(dx,dy);
+    cout << "dx = " << dx << " dy = " << dy << endl;
 
     return 0;
 }
