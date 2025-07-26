f4ff15ab76297de12316d37c7165b8aa0c1c67a8
Stefan Diaconu <stefan.diaconu@outlook.com>
2025-07-26 20:42:05 +0100

    Declare and define new functions in exercise 2 of the Drill

--- Chapter_08/Drill/my.cpp
+++ Chapter_08/Drill/my.cpp
@@ -15,3 +15,27 @@ void print(int i)
 {
     cout << i << endl;
 }
+
+void swap_v(int a,int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap_r(int &a,int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap_cr(const int &a,const int &b)
+{
+    int temp;
+    temp = a;
+    // a = b;
+    // b = temp;
+}
--- Chapter_08/Drill/my.h
+++ Chapter_08/Drill/my.h
@@ -9,4 +9,8 @@ extern int foo;
 void print_foo();
 void print(int);
 
+void swap_v(int,int);
+void swap_r(int&,int&);
+void swap_cr(const int&,const int&);
+
 #endif //MY_H
